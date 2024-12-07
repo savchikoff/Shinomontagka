7b13e5fbb4e3bb797bd4971d1cc78153c1436ee4
Daniil Savchik <savchik.official@gmail.com>
2024-12-07 13:30:27 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Router from './components/Router/Router';
 
 function App() {
+  useEffect(() => {
+    window.process = {
+      ...window.process,
+    };
+  }, []);
+
   return (
     <>
       <Router />
