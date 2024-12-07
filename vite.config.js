b527c8f69942fdde7554595b9de295f8b69757dd
Daniil Savchik <savchik.official@gmail.com>
2024-12-07 13:27:59 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
   return {
     define: {
-      'process.env': env
+      'process.env': {}
     },
     plugins: [react()],
   }
